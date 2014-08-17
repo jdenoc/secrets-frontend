cc71034e635ae492f16100296315570c659ef0f6
jdenoc <jdenoc@gmail.com>
2014-08-17 12:39:38 -0500

    * Improved error logging. * Unsetting "password" element of secret data array when saving.

--- includes/request_data.php
+++ includes/request_data.php
@@ -9,20 +9,19 @@ require_once(__DIR__.'/process_data.php');
 if(empty($_REQUEST['user'])){
     exit;
 } else {
-    $user = $_REQUEST['user'];
-    ProcessData::set_user_id($user);
+    ProcessData::set_user_id($_REQUEST['user']);
 }
 
 switch($_REQUEST['type']){
     case 'count':
-        $uri = 'count/id/'.$user;
+        $uri = 'count/id/'.ProcessData::get_user_id();
         $post = false;
         $post_data = array();
         $callback = 'do_nothing';
         break;
 
     case 'delete':
-        $uri = 'delete/id/'.ProcessData::clean_input('secret_id').'/user/'.$user;
+        $uri = 'delete/id/'.ProcessData::clean_input('secret_id').'/user/'.ProcessData::get_user_id();
         $post = false;
         $post_data = array();
         $callback = 'do_nothing';
@@ -31,7 +30,7 @@ switch($_REQUEST['type']){
     case 'list':
         $limit = empty($_POST['limit']) ? 50 : $_POST['limit'];
         $start = intval(ProcessData::clean_input('start'));
-        $uri = 'list/id/'.$user.'/start/'.$start.'/limit/'.$limit;
+        $uri = 'list/id/'.ProcessData::get_user_id().'/start/'.$start.'/limit/'.$limit;
         $post = false;
         $post_data = array();
         $callback = 'list_secrets';
@@ -39,7 +38,7 @@ switch($_REQUEST['type']){
 
     case 'get':
         $id = intval(ProcessData::clean_input('secret_id'));
-        $uri = 'display/id/'.$id.'/user/'.$user;
+        $uri = 'display/id/'.$id.'/user/'.ProcessData::get_user_id();
         $post = false;
         $post_data = array();
         $callback = 'display';
@@ -51,11 +50,11 @@ switch($_REQUEST['type']){
         $secret_data = json_decode(ProcessData::clean_input('data'), true);
         if(!empty($secret_data['password'])){
             $secret_data['password_length'] = strlen($secret_data['password']);
-            $secret_data['encrypted_password'] = ProcessData::encrypt($user, $secret_data['password'].$_GET['x']);
-            unset($secret_data['password'], $encrypt_url);
+            $secret_data['encrypted_password'] = ProcessData::encrypt(ProcessData::get_user_id(), $secret_data['password'].$_GET['x']);
         }
+        unset($secret_data['password']);
         $post_data = array(
-            'user'=>$user,
+            'user'=>ProcessData::get_user_id(),
             'data'=>base64_encode(json_encode($secret_data))
         );
         $callback = 'do_nothing';
@@ -63,7 +62,7 @@ switch($_REQUEST['type']){
 
     case 'password':
         $id = intval(ProcessData::clean_input('secret_id'));
-        $uri = 'password/id/'.$id.'/user/'.$user;
+        $uri = 'password/id/'.$id.'/user/'.ProcessData::get_user_id();
         $post = false;
         $post_data = array();
         $callback = 'get_password';
@@ -74,10 +73,10 @@ switch($_REQUEST['type']){
         $callback = 'do_nothing';
 }
 
-$api_url = 'http://services.local/index.php/api/secrets/';
+$api_url = 'http://services.local/api/secrets/';
 $json_response = ProcessData::make_call($api_url.$uri, $post, $post_data);
 if(!$response_array = json_decode($json_response, true)){
-    error_log(ProcessData::$error_title.$json_response);
+    error_log(ProcessData::$error_title."failed JSON response\n".$json_response);
 } else {
     if(empty($response_array['error'])){
         $response = call_user_func(array('ProcessData', $callback), $response_array['result']);
